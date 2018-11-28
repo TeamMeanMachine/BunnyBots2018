43459a75027053ec3bd7db96aa8e510ce0088d62
letsguo <Gabriel.w.guo@gmail.com>
2018-11-27 20:46:09 -0800

    Uptake and Ballsorter progess

--- src/main/kotlin/org/team2471/bunnybots2018/Ballsorter.kt
+++ src/main/kotlin/org/team2471/bunnybots2018/Ballsorter.kt
@@ -2,20 +2,54 @@ package org.team2471.bunnybots2018
 
 import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.Solenoid
+import org.team2471.frc.lib.coroutines.periodic
+import org.team2471.frc.lib.framework.DaemonSubsystem
 
-object Ballsorter {
+object Ballsorter : DaemonSubsystem("Ballsorter") {
     private val lowerHorizontalBelt = TalonSRX(TODO())
     private val verticalBelt = TalonSRX(TODO())
     private val ballPusher = Solenoid(TODO())
-//    private val topHorizontalBelt = TalonSRX(TODO())
+    private val colorSensor = DigitalInput(TODO())
+    private val topHorizontalBelt = TalonSRX(TODO())
 
-    fun uptake (speed: Double) {
-        lowerHorizontalBelt.set(ControlMode.PercentOutput, speed)
-        verticalBelt.set(ControlMode.PercentOutput, speed)
+    val lastBallColor: Color
+        get() = if (colorSensor.get()) Color.RED else Color.BLUE
+
+    fun uptake (uptakeDirection: UptakeDirection) {
+        lowerHorizontalBelt.set(ControlMode.PercentOutput, 1.0)
+        verticalBelt.set(ControlMode.PercentOutput, 1.0)
+
+        topHorizontalBelt.set(ControlMode.PercentOutput, if (uptakeDirection == UptakeDirection.LEFT) -1.0 else 1.0)
+
+        ballPusher.set(lastBallColor == Color.BLUE)
     }
 
-    fun pushBall (eject: Boolean){
-        ballPusher.set(eject)
+    fun stop(){
+        lowerHorizontalBelt.set(ControlMode.PercentOutput, 0.0)
+        verticalBelt.set(ControlMode.PercentOutput, 0.0)
+        topHorizontalBelt.set(ControlMode.PercentOutput, 0.0)
+        ballPusher.set(false)
+    }
+
+    override suspend fun default() {
+        periodic {
+            val direction = OI.uptakeDirection
+
+            if (direction != null) uptake(direction) else stop()
+
+            false
+        }
+    }
+
+    enum class Color {
+        RED,
+        BLUE,
+    }
+
+    enum class UptakeDirection {
+        LEFT,
+        RIGHT,
     }
 }
--- src/main/kotlin/org/team2471/bunnybots2018/OI.kt
+++ src/main/kotlin/org/team2471/bunnybots2018/OI.kt
@@ -2,6 +2,7 @@ package org.team2471.bunnybots2018
 
 import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.XboxController
+import org.team2471.frc.lib.math.deadband
 
 object OI {
     private val driverController = XboxController(0)
@@ -12,4 +13,19 @@ object OI {
 
     val driveTurn: Double
         get() = driverController.getX(GenericHID.Hand.kRight)
+
+    val uptakeDirection: Ballsorter.UptakeDirection?
+        get() {
+            val left = operatorController.getTriggerAxis(GenericHID.Hand.kLeft)
+                    .deadband(0.2)
+            val right = operatorController.getTriggerAxis(GenericHID.Hand.kRight)
+                    .deadband(0.2)
+
+            return when {
+                left > right -> Ballsorter.UptakeDirection.LEFT
+                right > left -> Ballsorter.UptakeDirection.RIGHT
+                else -> null
+            }
+        }
+
 }
